001df673a17a04bbc424114a725de898b50a8d8e
lilman109 <akira.yoshidasjsu@gmail.com>
2021-04-30 00:01:26 +0900

    implemented when to display next question button

--- src/App.tsx
+++ src/App.tsx
@@ -66,9 +66,14 @@ const App = () => {
 					callback={checkAnswer}
 				/>
 			)}
-			<button className='next_question' onClick={nextQuestion}>
-				Next Question
-			</button>
+			{!gameOver &&
+			!loading &&
+			userAnswers.length === number + 1 &&
+			number !== TOTAL_QUESTIONS - 1 ? (
+				<button className='next_question' onClick={nextQuestion}>
+					Next Question
+				</button>
+			) : null}
 		</div>
 	);
 };
